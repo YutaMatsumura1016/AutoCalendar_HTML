c139a5c7ab02fe3b4f19c6f68a090683467e2d55
YutaMatsumura1016 <yuta.train1016@gmail.com>
2023-10-06 20:34:16 +0900

    start coding

--- AutoCalApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from AutoCalApp import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('top/', views.formView, name='formView'),
-    path('top/', views.topView, name='topView'),
-    path('result/', views.resultView, name='resultView'),
-]
